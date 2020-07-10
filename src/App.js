745568b899ef823eff2e36ed6767d2f622a610c7
Sergej Vlasov <sergej.s.vlasov@gmail.com>
2020-07-10 22:32:00 +0100

    removed authintication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { API, graphqlOperation } from "aws-amplify";
-import { withAuthenticator } from "@aws-amplify/ui-react";
 
 import { createTodo } from "./graphql/mutations";
 import { listTodos } from "./graphql/queries";
@@ -98,4 +97,4 @@ const styles = {
   }
 };
 
-export default withAuthenticator(App);
+export default App;
